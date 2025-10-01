e9ceef74322bc7946d210d861bcc532904983230
ParkChangSun <changsun19991212@gmail.com>
2025-10-01 23:55:48 +0900

    fix: marked to mdsvex

--- src/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export async function getPosts() {
-    return [{ title: 'Hello World', content: 'This is my first post!' }];
-}
--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
+import {mdsvex} from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true
 		}),
-		prerender: {}
-	}
+	},
+	preprocess: [mdsvex({
+		extensions: ['.md']
+	})],
+	extensions: ['.svelte', '.md']
 };
 
 export default config;

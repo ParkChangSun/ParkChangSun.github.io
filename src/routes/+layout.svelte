f22114e6e02d2ef3eaeae50bca33e076257309e4
ParkChangSun <changsun19991212@gmail.com>
2025-10-20 23:57:33 +0900

    draft: baek7579.md

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,9 +25,9 @@
 			<span class="text-blue-500">&lt;</span>DevLog<span class="text-blue-500">/&gt;</span>
 		</h1>
 		<nav class="flex gap-6 text-lg">
-			<a href="/" class="transition hover:text-blue-500">Home</a>
+			<!-- <a href="/" class="transition hover:text-blue-500">Home</a>
 			<a href="/tags" class="transition hover:text-blue-500">Tags</a>
-			<a href="/about" class="transition hover:text-blue-500">About</a>
+			<a href="/about" class="transition hover:text-blue-500">About</a> -->
 			<!-- <button on:click={toggleTheme} class="ml-4 p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700">
         {#if dark}
           <Sun size="20" />
@@ -44,7 +44,7 @@
 			<h2 class="mb-10 text-center text-4xl font-bold">Latest Posts</h2>
 			<div class="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
 				{#each Array(6) as _, i}
-					<a
+					<!-- <a
 						href={`/blog/post-${i}`}
 						class="block rounded-2xl border border-transparent bg-white p-6 shadow transition hover:border-blue-500 hover:shadow-xl dark:bg-gray-800"
 					>
@@ -53,7 +53,7 @@
 							This is a short summary of the post, giving a preview of its content.
 						</p>
 						<span class="text-sm font-semibold text-blue-500">Read more →</span>
-					</a>
+					</a> -->
 				{/each}
 			</div>
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,4 @@
-<script lang="ts">
-	const { data } = $props();
-	const { posts } = data;
-</script>
-
 <h1>Welcome to ParkChangSun.github.io</h1>
-
-{#each posts as p}
-	<a href={`/posts/${p}`}>{p}</a>
-{/each}
 
 <p>note - pnpm svelte init (sv) plugins</p>
 edgeless graph making problem 그 뭐냐 10217 같은 다익 문제 풀때 힙써보고 또 메모리초과나는지 보기 remarklint?
-블로그글쓰기를 글쓰기
